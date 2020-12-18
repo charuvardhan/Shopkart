8ac30b71b13867134bbcd2eab48adfcb97370145
Charu <vardhancharu8@gmail.com>
2020-12-18 23:08:29 +0530

    shopkart

---
+++ backend/build/precache-manifest.45a5f5792e9f3f5588a47258cc73ade9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb15a8bd16c51914edc7b0ac496cfeee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f97a405f559daa62661",
+    "url": "/static/css/main.50315afc.chunk.css"
+  },
+  {
+    "revision": "19391c612900dfb1dc62",
+    "url": "/static/js/2.4aefa8bd.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.4aefa8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f97a405f559daa62661",
+    "url": "/static/js/main.0ace3183.chunk.js"
+  },
+  {
+    "revision": "938b36141f154c9021bf",
+    "url": "/static/js/runtime-main.643ffef5.js"
+  }
+]);
